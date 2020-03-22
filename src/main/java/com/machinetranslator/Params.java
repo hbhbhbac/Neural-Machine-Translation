dfa914d57f36d31f79a2c56ec76a71b50bd81c75
Kenfack Arnaud <arnaudkenfack@yahoo.fr>
2020-03-22 16:03:58 +0100

    Add files via upload

--- src/main/java/com/machinetranslator/Params.java
+++ src/main/java/com/machinetranslator/Params.java
@@ -25,20 +25,14 @@ public class Params {
 	
 	// Parameters for the training phase (hyper parameters)
 	private int batchSize = 20;  
-	private int nEpochs = 200;
+	private int nEpochs = 120;
 	
 	private int FeatureVectorSize = 88;  // This is the size of the one hot vector
-	                                     // Length of this one hot vector is 14 (10 digits and "+"," ",beginning of string and end of string, ....)
-	private int maxCharForInputs = 20;  // To try out addition for numbers with different number of digits simply change "numDigits"
+	                                     
+	private int maxCharForInputs = 20;  
 	private int maxCharForOutputs = 50;
 	private int numHiddenNodes = 256; 
 	private int seed = 1234;
-	
-	// Parameters of the CSV file
-	private String commaDelimiter = ",";
-	// File attributes index
-	private int frenchIdx = 0;
-	private int englishIdx = 1;
 	
 	// Data directory. The model file will be saved here.
     private String dataDir = System.getProperty("user.dir")+ "/src/main/resources/data/";
@@ -89,18 +83,6 @@ public class Params {
 	
 	public int getSeed() {
 		return seed;
-	}
-	
-	public String getCommaDelimiter() {
-		return commaDelimiter;
-	}
-	
-	public int getFrenchIdx() {
-		return frenchIdx;
-	}
-	
-	public int getEnglishIdx() {
-		return englishIdx;
 	}
 	
 	public String getDataDir() {
