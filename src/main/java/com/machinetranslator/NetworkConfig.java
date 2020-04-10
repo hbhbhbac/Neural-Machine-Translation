60f52af0c80f1697710214563f11cebfd3d97da6
Kenfack Arnaud <arnaudkenfack@yahoo.fr>
2020-04-10 18:25:52 +0100

    Update NetworkConfig.java

--- src/main/java/com/machinetranslator/NetworkConfig.java
+++ src/main/java/com/machinetranslator/NetworkConfig.java
@@ -26,46 +26,46 @@ public class NetworkConfig {
 	public ComputationGraph getNetworkConfig() {
 		
 		ComputationGraphConfiguration conf = new NeuralNetConfiguration.Builder()
-				.seed(params.getSeed())
-				.weightInit(params.getWeightInit())
-				.updater(params.getUpdater())
-				.cacheMode(params.getCacheMode())
-                .trainingWorkspaceMode(params.getWorkspaceMode())
-                .inferenceWorkspaceMode(params.getWorkspaceMode())
-				.graphBuilder()
-				.setInputTypes(InputType.recurrent(params.getFeatureVectorSize()), InputType.recurrent(params.getFeatureVectorSize()))
+			.seed(params.getSeed())
+			.weightInit(params.getWeightInit())
+			.updater(params.getUpdater())
+			.cacheMode(params.getCacheMode())
+                        .trainingWorkspaceMode(params.getWorkspaceMode())
+                        .inferenceWorkspaceMode(params.getWorkspaceMode())
+			.graphBuilder()
+			.setInputTypes(InputType.recurrent(params.getFeatureVectorSize()), InputType.recurrent(params.getFeatureVectorSize()))
 				
-				//The 2 inputs of the network 
-                .addInputs("englishIn", "frenchOut")
+			//The 2 inputs of the network 
+                        .addInputs("englishIn", "frenchOut")
                        
-                //All the inputs to the encoder will have size = batchSize * FeatureVectorSize * timesteps 
-                .addLayer("encoder", new LSTM.Builder()
+                         //All the inputs to the encoder will have size = batchSize * FeatureVectorSize * timesteps 
+                        .addLayer("encoder", new LSTM.Builder()
                 		.nIn(params.getFeatureVectorSize())
                 		.nOut(params.getNumHiddenNodes())
                 		.activation(Activation.SOFTSIGN)
                 		.build(), "englishIn")
                 
-                //Vertex indicating the very last time step of the encoder layer
-                .addVertex("lastTimeStep", new LastTimeStepVertex("englishIn"), "encoder")
+                         //Vertex indicating the very last time step of the encoder layer
+                         .addVertex("lastTimeStep", new LastTimeStepVertex("englishIn"), "encoder")
                 
-                //Create a vertex that allows the duplication of 2d input to a 3d input
-                .addVertex("duplicateTimeStep", new DuplicateToTimeSeriesVertex("frenchOut"), "lastTimeStep")
+                         //Create a vertex that allows the duplication of 2d input to a 3d input
+                         .addVertex("duplicateTimeStep", new DuplicateToTimeSeriesVertex("frenchOut"), "lastTimeStep")
                 
-                //The inputs to the decoder will have size = size of output of last timestep of encoder (numHiddenNodes) + size of the other input
-                .addLayer("decoder", new LSTM.Builder()
+                         //The inputs to the decoder will have size = size of output of last timestep of encoder (numHiddenNodes) + size of the other input
+                         .addLayer("decoder", new LSTM.Builder()
                 		.nIn(params.getFeatureVectorSize() + params.getNumHiddenNodes())
                 		.nOut(params.getNumHiddenNodes())
                 		.activation(Activation.SOFTSIGN)
                 		.build(), "frenchOut", "duplicateTimeStep")
                 
-                .addLayer("output", new RnnOutputLayer.Builder()
+                          .addLayer("output", new RnnOutputLayer.Builder()
                 		.nIn(params.getNumHiddenNodes())
                 		.nOut(params.getFeatureVectorSize())
                 		.activation(Activation.SOFTMAX).lossFunction(LossFunctions.LossFunction.MCXENT)
                 		.build(), "decoder")
                 
                 .setOutputs("output")
-				.build();
+		.build();
 		
 		return new ComputationGraph(conf);
 	}
